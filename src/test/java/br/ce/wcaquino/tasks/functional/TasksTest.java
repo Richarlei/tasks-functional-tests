f6cb2192178aa8bd566ce63e4f7b34b9b0c9b5c9
Richarlei Hederique Reis <richarlei.reis@agoratech.local>
2020-04-23 13:48:51 -0300

    Paralelismo Docker2

--- src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -17,8 +17,8 @@ public class TasksTest {
 	public WebDriver acessarAplicacao() throws MalformedURLException {
 		//WebDriver driver = new ChromeDriver();
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
-		WebDriver driver = new RemoteWebDriver(new URL("http://144.15.30.49:4444/wd/hub"),cap);
-		driver.navigate().to("http://144.15.30.49:8001/tasks/");
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.15.26:4444/wd/hub"),cap);
+		driver.navigate().to("http://192.168.15.26:8001/tasks/");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 		
