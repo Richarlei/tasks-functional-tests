70a2ecf7689e4a00520dc84e890baf987506e77f
Richarlei Hederique Reis <richarlei.reis@agoratech.local>
2020-05-04 20:29:19 -0300

    Troca de IP

--- src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -17,8 +17,8 @@ public class TasksTest {
 	public WebDriver acessarAplicacao() throws MalformedURLException {
 		//WebDriver driver = new ChromeDriver();
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
-		WebDriver driver = new RemoteWebDriver(new URL("http://10.4.0.57:4444/wd/hub"),cap);
-		driver.navigate().to("http://10.4.0.57:8001/tasks/");
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.15.10:4444/wd/hub"),cap);
+		driver.navigate().to("http://192.168.15.10:8001/tasks/");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 		
